d1912f3823e802c8fec98d73f35a0c67e7d00b94
Svaym <Vaymeraa22@mail.ru>
2024-06-19 17:21:27 +0500

    add base

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens : {
+      "5xl" : { max: "1920px" },
+      "4xl" : { max: "1870px" },
+      "3xl" : { max: "1680px" },
+      "2xl" : { max: "1400px" },
+      "xl" : { max: "1200px" },
+      "lg" : { max: "992px" },
+      "md" : { max: "768px" },
+      "sm" : { max: "576px" },
+      "xs" : { max: "450px" },
+      "xxs" : { max: "375px" },
+    },
+    extend: {
+      colors: {
+        'marineBlue': '#02295a',
+        'purplishBlue': '#473dff',
+        'pastelBlue': '#adbeff',
+        'lightBlue': '#bfe2fd',
+        'strawberryRed': '#ed3548',
+        'coolGray': '#9699ab',
+        'lightGray': '#d6d9e6',
+        'magnolia': '#f0f6ff',
+        'alabaster': '#fafbff',
+      },
+      fontFamily: {
+        'ubuntu': ['Ubuntu', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
